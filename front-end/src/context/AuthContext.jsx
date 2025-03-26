27d8ac0bfdda096aaa95de8bda8475c92c4a506a
TuanHoang25 <143671642+TuanHoang25@users.noreply.github.com>
2025-03-26 22:36:30 +0700

    Update AuthContext.jsx API

--- front-end/src/context/AuthContext.jsx
+++ front-end/src/context/AuthContext.jsx
@@ -13,7 +13,7 @@ const AuthContext = ({ children }) => {
         const token = localStorage.getItem("token");
         if (token) {
           const response = await axios.get(
-            "http://localhost:3000/api/auth/verify",
+            "https://do-an-chuyen-nganh-cnpm.onrender.com/api/auth/verify",
             {
               headers: {
                 Authorization: `Bearer ${token}`,
