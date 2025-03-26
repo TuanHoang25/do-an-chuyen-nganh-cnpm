544976098c4f8d689df9ba1a62a2e2a7d4a0bd7b
TuanHoang25 <143671642+TuanHoang25@users.noreply.github.com>
2025-03-26 23:17:48 +0700

    Update index.js API cors

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["https://frontend-2vqh.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
